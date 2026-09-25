bbf1a4ef8b87a015bf3657128066fd6ac666d1a6
Yhara Landi <landiyhara@gmail.com>
2026-09-25 19:30:51 +0200

    fix

--- be/src/main/java/org/example/showroom/security/SecurityConfig.java
+++ be/src/main/java/org/example/showroom/security/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.jose.MacAlgorithm;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
